f02f7052a9471b0329e84abddfe7c1fbcfa72341
Igna Ion Resmana <igna.resmana@gmail.com>
2020-01-03 14:13:18 +0700

    initial commit. with entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,118 @@
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="categories")
+ */
+class Category
+{
+    // properties
+    private $id;
+    private $name;
+    
+    // constructor
+    public function __construct()
+    {
+        $this->jobs = new ArrayCollection();
+        $this->affiliates = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId() : ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name) : self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Job[]|ArrayCollection
+     */
+    public function getJobs()
+    {
+        return $this->jobs;
+    }
+
+    /**
+     * @param Job $job
+     *
+     * @return self
+     */
+    public function addJob(Job $job) : self
+    {
+        if (!$this->jobs->contains($job)) {
+            $this->jobs->add($job);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Job $job
+     *
+     * @return self
+     */
+    public function removeJob(Job $job) : self
+    {
+        $this->jobs->removeElement($job);
+
+        return $this;
+    }
+
+    /**
+     * @return Affiliate[]|ArrayCollection
+     */
+    public function getAffiliates()
+    {
+        return $this->affiliates;
+    }
+
+    /**
+     * @param Affiliate $affiliate
+     *
+     * @return self
+     */
+    public function addAffiliate($affiliate) : self
+    {
+        if (!$this->affiliates->contains($affiliate)) {
+            $this->affiliates->add($affiliate);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Affiliate $affiliate
+     *
+     * @return self
+     */
+    public function removeAffiliate($affiliate) : self
+    {
+        $this->affiliates->removeElement($affiliate);
+
+        return $this;
+    }
+}
